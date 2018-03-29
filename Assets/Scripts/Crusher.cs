f97106dcda95ba794d042eef2c94a563098faa67
Logan Payton <Logan.Payton@javs.com>
2018-03-29 15:32:03 -0400

    Add mask guy

---
+++ Assets/Scripts/Crusher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Crusher : BasicEnemy
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            // sets initial velocity toward the hero
+            var hero = GameObject.Find("Hero");
+            if (hero != null)
+            {
+                Velocity = (hero.transform.position - transform.position).normalized * MoveSpeed;
+            }
+        }
+    }
+}
