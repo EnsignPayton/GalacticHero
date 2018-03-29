002920423af850310162cb3823ac9839e089640e
Logan Payton <Logan.Payton@javs.com>
2018-03-29 16:11:14 -0400

    Update crusher ai and some upkeep

--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -4,15 +4,36 @@ namespace Assets.Scripts
 {
     public class Crusher : BasicEnemy
     {
+        private Hero _hero;
+
         protected override void Start()
         {
             base.Start();
 
             // sets initial velocity toward the hero
-            var hero = GameObject.Find("Hero");
-            if (hero != null)
+            _hero = FindObjectOfType<Hero>();
+            if (_hero != null)
             {
-                Velocity = (hero.transform.position - transform.position).normalized * MoveSpeed;
+                Velocity = (_hero.transform.position - transform.position).normalized * MoveSpeed;
+            }
+        }
+
+        protected override void SetVelocity()
+        {
+            if (_hero != null)
+            {
+                var position = Camera.main.WorldToViewportPoint(transform.position);
+
+                if (position.x <= ScreenBoundOffset || position.x >= 1.0f - ScreenBoundOffset ||
+                    position.y <= ScreenBoundOffset || position.y >= 1.0f - ScreenBoundOffset)
+                {
+                    Velocity = (_hero.transform.position - transform.position).normalized * MoveSpeed;
+                }
+            }
+            else
+            {
+                // Only call when no hero is found
+                base.SetVelocity();
             }
         }
     }
