0f9ebedaa0641846bb51c3a630692c016be328b8
Logan Payton <Logan.Payton@javs.com>
2018-04-13 10:44:31 -0400

    Silencer core animation

--- Assets/Scripts/Silencer.cs
+++ Assets/Scripts/Silencer.cs
@@ -5,5 +5,24 @@ namespace Assets.Scripts
 {
     public class Silencer : Script
     {
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        private IEnumerator PeriodicMovement()
+        {
+            // Or while enabled, while alive, etc.
+            while (true)
+            {
+                // Move in first arc
+
+                // Move in second arc
+
+                // Move in third arc
+
+                // Stop, thn repeat
+            }
+        }
     }
 }
